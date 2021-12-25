b5bf20f5d0914abec2146daea1295ea16c31d65c
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2021-12-25 13:13:30 -0500

    Geolocation basic functionalities created

--- src/js/classes/app.js
+++ src/js/classes/app.js
@@ -1,11 +1,14 @@
 import { sidebar } from '../selectors.js';
 import { handleSidebar } from '../functions.js';
+import { getCurrentWeather } from '../currentWeather.js';
 
 class App {
 	constructor() {
 		document.addEventListener('DOMContentLoaded', () => {
 			sidebar.addEventListener('click', handleSidebar);
 		});
+
+		getCurrentWeather();
 	}
 }
 
